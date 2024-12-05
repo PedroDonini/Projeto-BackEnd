863d41b15ed11b6c28173b3bddc8833c6c930334
PedroDonini <pedro.donini96@gmail.com>
2024-12-05 16:51:07 -0300

    Criação dos arquivos de rotas

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '../swagger-output.json';
 
+import veiculosRoutes from './routes/veiculos.routes.js';
+import pecasRoutes from './routes/pecas.routes.js';
+import authRoutes from './routes/auth.routes.js';
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -10,6 +14,11 @@ app.use(express.json());
 
 // Documentação Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Rotas
+app.use('/veiculos', veiculosRoutes);
+app.use('/pecas', pecasRoutes) ;
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
