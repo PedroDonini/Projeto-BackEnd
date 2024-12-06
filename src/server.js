90030e35301582c15187f8cd2f19166fc3ba4faa
PedroDonini <pedro.donini96@gmail.com>
2024-12-06 00:16:38 -0300

    Criação de middleware e rota de login

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import swaggerDocument from './config/swagger-output.json' assert { type: 'json'
 
 import veiculosRoutes from './routes/veiculos.routes.js';
 import usuariosRoutes from './routes/usuarios.routes.js';
+import authRoutes from './routes/auth.routes.js' ;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,6 +20,8 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Rotas
 app.use('/veiculos', veiculosRoutes);
 app.use(usuariosRoutes);
+app.use(authRoutes) ;
+
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
