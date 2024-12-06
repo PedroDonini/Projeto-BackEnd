88d910ed16abfa57e02b3e78da64f4aa9472eabe
PedroDonini <pedro.donini96@gmail.com>
2024-12-05 22:58:12 -0300

    Mudanças no arquivo server e criação do model de veículo

---
+++ src/models/veiculo.model.js
@@ -0,0 +1,2 @@
+// Aqui vamos armazenar os veículos, até que seja criado um banco de dados.
+export const veiculos = [];
--- src/server.js
+++ src/server.js
@@ -1,24 +1,22 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../swagger-output.json';
+import swaggerDocument from './config/swagger-output.json' assert { type: 'json' } ;
 
 import veiculosRoutes from './routes/veiculos.routes.js';
-import pecasRoutes from './routes/pecas.routes.js';
-import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(express.json());
+app.use(bodyParser.json());
 
 // Documentação Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Rotas
 app.use('/veiculos', veiculosRoutes);
-app.use('/pecas', pecasRoutes) ;
-app.use('/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
